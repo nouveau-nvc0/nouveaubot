f67750fea88b77f69114698cdb0c499fd0c7c2de
nouveau-nvc0 <42435266+nouveau-nvc0@users.noreply.github.com>
2025-09-06 10:02:44 +0300

    rename

--- bot/route.py
+++ bot/route.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 basilbot contributors
+# Copyright (C) 2024 nouveaubot contributors
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
